617cb8fb23cb33cbf427224b80ab63c57eadcf4d
MridulSheikh <prince9nazir@gmail.com>
2023-01-17 09:14:14 +0600

    fixed error

--- src/pages/nft/[id].tsx
+++ src/pages/nft/[id].tsx
@@ -33,17 +33,17 @@ const Nftdropepage = () => {
             </span>{" "}
             NFT Market Place
           </h1>
-          <button onClick={address ? disconnect : connectwithMetamask} className="rounded-full bg-rose-400 px-4 py-2 text-white text-xs font-bold lg:text-base">{address ? "Sign Out" : "Sing In"}</button>
+          <button onClick={address ? disconnect : connectwithMetamask} className="rounded-full bg-rose-400 px-4 py-2 text-white text-xs font-bold lg:text-base">{address ? `Sign Out` : `Sing In`}</button>
         </header>
         {
             address && (
-                <p className="text-center text-sm text-rose-400 mt-3">You're Logged in with wallet {address.substring(0,5)}...{address.substring(5,address.length)}</p>
+                <p className="text-center text-sm text-rose-400 mt-3">Your&apos;e Logged in with wallet {address.substring(0,5)}...{address.substring(5,8)}</p>
             )
         }
         <div className=" py-10 lg:py-24 flex flex-1 flex-col items-center space-y-6 text-center lg:space-y-0 lg:justify-center">
             <img className="w-80 object-cover pb-10 lg:h-40" src="https://links.papareact.com/bdy" alt="" />
             <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold">The Nazir Ape Coding club | NFT Drop</h1>
-            <span className="pt-2 text-xl text-green-500">25/50 NFT's claimed</span>
+            <span className="pt-2 text-xl text-green-500">25/50 NFT&apos;s claimed</span>
         </div>
         <button className="w-full py-2 text-xs lg:text-base bg-rose-400 rounded-full font-bold text-white hover:bg-rose-500 ">MINT (0.01 ETH)</button>
       </div>
